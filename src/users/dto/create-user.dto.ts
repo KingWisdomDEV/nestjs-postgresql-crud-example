4f9ae5c15e9acdb8f5a6cfdac27c3db898f7a0ce
King Wisdom <tue.hoang@icetea.io>
2024-04-09 02:30:17 +0700

    feat: users crud api

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserDto {
+    walletAddress: string;
+    description: string;
+    active: boolean;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ){}
+
+ async create(createUserDto: CreateUserDto) {
+    const user = this.usersRepository.create(createUserDto)
+
+    
+    return await this.usersRepository.save(user)
+  }
+
+  async findAll() {
+    return await this.usersRepository.find()
+  }
+
+  async findOne(id: number) {
+    return await this.usersRepository.findOne({where: {id}})
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+    if(!user) {
+      throw new NotFoundException()
+
+
+    }
+
+    Object.assign(user, updateUserDto)
+
+    return await this.usersRepository.save(user)
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if(!user) {
+      throw new NotFoundException()
+
+
+    }
+
+
+    return await this.usersRepository.remove(user)
+  }
+}
